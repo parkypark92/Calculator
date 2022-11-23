1cdd4076750e5e90819cfd989d9ae454a8848491
Andrew <115462877+parkypark92@users.noreply.github.com>
2022-11-23 12:54:07 +0000

    edit calculate() function and add displayAnswer() function for '=' sign

--- script.js
+++ script.js
@@ -17,43 +17,45 @@ const numArray = [zero, one, two, three, four, five, six, seven, eight, nine];
 for(let i = 0; i < numArray.length; i++)
 {
     numArray[i].addEventListener('click', () => {
-        if(display.textContent.length < 30) {
+        if(display.textContent.length < 20) {
         display.textContent += `${i}`;
+        displayTop.textContent += `${i}`;
         }
     });
 }
 
 const dot = document.querySelector('.dot');
 dot.addEventListener('click', () => {
-    if(display.textContent.length < 22){
+    if(display.textContent.length < 20){
 display.textContent += '.';
+displayTop.textContent += '.';
     }
 });
 
 const plus = document.querySelector('.plus');
 plus.addEventListener('click', () => {
-    toTop(display);
+    display.textContent = "";
     calculate(displayTop, ' + ' );
     
 });
 
 const minus = document.querySelector('.minus');
 minus.addEventListener('click', () => {
-    toTop(display);
+    display.textContent = "";
     calculate(displayTop, ' - ' );
 
 });
 
 const star = document.querySelector('.star');
 star.addEventListener('click', () => {
-    toTop(display);
+    display.textContent = "";
     calculate(displayTop, ' * ' );
 
 });
 
 const slash = document.querySelector('.slash');
 slash.addEventListener('click', () => {
-    toTop(display);
+    display.textContent = "";
     calculate(displayTop, ' / ' );
 
 });
@@ -66,24 +68,30 @@ displayTop.textContent = "";
 
 const equals = document.querySelector('.equals');
 equals.addEventListener('click', () => {
-let calcArray = display.textContent.split(' ');
-display.textContent = calculate(calcArray);
+    displayAnswer(displayTop);
 });
 
 
 function calculate(sumDisplay, operator)
 {
     let currentSum = sumDisplay.textContent.split(' ');
+    
     if(currentSum.length != 3) {
         sumDisplay.textContent += operator;
         return;
+    } 
+    else if(currentSum[2] == "" ){
+        return;
     } else {
         let result = operate(currentSum[0], currentSum[1], currentSum[2]);
-        sumDisplay.textContent += operator;
-        display.textContent = '= ' + result;
-
-
+        sumDisplay.textContent = result + operator;
     }
+}
+
+function displayAnswer(sumDisplay) {
+    let currentSum = sumDisplay.textContent.split(' ');
+    let result = operate(currentSum[0], currentSum[1], currentSum[2]);
+    display.textContent = "= " + result;
 }
 
 
@@ -118,8 +126,8 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function toTop(display) {
-    displayTop.textContent += display.textContent;
-    display.textContent = "";
-}
+// function toTop(display) {
+//     displayTop.textContent += display.textContent;
+//     display.textContent = "";
+// }
 
